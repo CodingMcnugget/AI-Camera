d902114be62f8dd73c08d0235632a858cde14eab
CodingMcnugget <144944897+CodingMcnugget@users.noreply.github.com>
2024-04-15 22:39:40 +0000

    This version is helped by Sam

--- index.js
+++ index.js
@@ -1,60 +1,84 @@
 import express from "express";
 import Replicate from "replicate";
-import fs from "fs";
+// import fs from "fs"; // i might not need this
 import path from "path";
-import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { fileURLToPath } from "url";
+import bodyparser from "body-parser";
+import multer from "multer";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 // Configure body parser for JSON including large encoded data
-app.use(express.json({ limit: "10mb" }));
+// app.use(express.json({ limit: "10mb" }));
+const Urlencoded = bodyparser.urlencoded({ extended: true });
+const upload = multer({ dest: "public/images" });
+
 // Serve static files from the public directory
 app.use(express.static("public"));
-// Serve uploaded files
-app.use("/uploads", express.static("uploads"));
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, "/public/images"));
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+// var upload = multer({ storage: storage });
 
+// Serve uploaded files
+// app.use("/uploads", express.static("uploads"));
+app.use(Urlencoded);
 const replicate = new Replicate();
 
-app.post("/api/image", async (req, res) => {
+app.post("/api/image", upload.single("my-file"), async (req, res) => {
   console.log("Received image for processing");
+  console.log(req.file);
+  // try {
 
-  try {
-    const imageData = req.body.image.replace(/^data:image\/\w+;base64,/, "");
-    const buffer = Buffer.from(imageData, "base64");
-    const filename = `tempImage_${Date.now()}.jpg`;
-    const filepath = path.join(__dirname, "uploads", filename);
-    fs.writeFileSync(filepath, buffer);
+  //   const imageUrl = `${req.protocol}://${req.get("host")}/img/${req.file.filename}`;
+  //   console.log(`Public URL for the uploaded image: ${imageUrl}`);
 
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${filename}`;
-    console.log(`Public URL for the uploaded image: ${imageUrl}`);
+  //   const output = await replicate.run(
+  //     "sujaykhandekar/object-removal:153b0087c2576ad30d8cbddb35275b387d1a6bf986bda5499948f843f6460faf",
+  //     {
+  //       input: {
+  //         image_path: imageUrl,
+  //         objects_to_remove: "person",
+  //         // input_image: imageUrl,
+  //         // image: imageUrl,
+  //         // mask_prompt: "Human",
+  //         // adjustment_factor: -25,
+  //         // negative_mask_prompt: "Wheels",
+  //       },
+  //     },
+  //   );
+  //   console.log("Replicate API call successful. Output:", output);
+  //   res.send({ result: output.result });
 
-    const output = await replicate.run(
-      "sujaykhandekar/object-removal:153b0087c2576ad30d8cbddb35275b387d1a6bf986bda5499948f843f6460faf",
-      {
-        input: {
-          image_path: imageUrl,
-          objects_to_remove: "person" 
-          // input_image: imageUrl,
-          // image: imageUrl,
-          // mask_prompt: "Human",
-          // adjustment_factor: -25,
-          // negative_mask_prompt: "Wheels",
-        },
-      },
-    );
-    console.log("Replicate API call successful. Output:", output);
-    res.send({ result: output.result });
-    
+  //   // Clean up the temporary image file after processing
+  //   fs.unlinkSync(filepath);
+  // } catch (error) {
+  //   console.error("Error during Replicate API call:", error);
+  //   res
+  //     .status(500)
+  //     .send({ error: "Failed to call Replicate API", details: error.message });
+  // }
+});
 
-    // Clean up the temporary image file after processing
-    fs.unlinkSync(filepath);
-  } catch (error) {
-    console.error("Error during Replicate API call:", error);
-    res
-      .status(500)
-      .send({ error: "Failed to call Replicate API", details: error.message });
-  }
+app.get("/img/:id", (req, res) => {
+  console.log(req.params.id);
+  // res.sendFile(path.join(__dirname, "public/images/") + req.params.id);
+// const imageData = req.body.image.replace(/^data:image\/\w+;base64,/, "");
+// const buffer = Buffer.from(imageData, "base64");
+// const filename = `tempImage_${Date.now()}.jpg`;
+// const filepath = path.join(__dirname, "uploads", filename);
+// fs.writeFileSync(filepath, buffer);
+  res.type("png").send(path.join(__dirname, "public/images/") + req.params.id);
+
 });
 
 app.listen(3000, () => {
--- public/script.js
+++ public/script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const video = document.getElementById("video");
   const captureButton = document.getElementById("processImage");
 
-  // 准备一个canvas来捕获和发送图像
+  // set up canvas
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
-  // 获取用户的媒体设备（前置或后置摄像头）
+  // get camera
   navigator.mediaDevices
     .getUserMedia({ video: true })
     .then((stream) => {
@@ -15,29 +15,35 @@ document.addEventListener("DOMContentLoaded", function () {
       console.error("Error accessing the camera", err);
     });
   captureButton.addEventListener("click", () => {
-    // 设置canvas尺寸与视频流一致
+    // set the canvas size same as video size
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
-    // 在canvas上绘制当前视频帧
+    // dislay the video on canvas
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    // 将canvas中的图像转换为数据URL（即base64编码的JPEG图像）
-    const imageDataUrl = canvas.toDataURL("image/jpeg");
-    // 使用fetch API发送数据URL到后端服务
-    fetch("/api/image", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ image: imageDataUrl }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.result);
-        const resultImage = document.getElementById("resultImage");
-        resultImage.src = data.result;
+    // convert the screen shot to base64 URL
+    // const imageDataUrl = canvas.toDataURL("image/jpeg");
+    canvas.toBlob((blob) => {
+      console.log(blob);
+      let formData = new FormData();
+      formData.append("my-file", blob, "filename.png");
+      // send the image to the server
+      fetch("/api/image", {
+        method: "POST",
+        // headers: {
+        //   "Content-Type": "multipart/form-data",
+        // },
+        // body: JSON.stringify({ image: imageDataUrl }),
+        body: formData,
       })
-      .catch((err) => {
-        console.error("Failed to send image", err);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.result);
+          const resultImage = document.getElementById("resultImage");
+          resultImage.src = data.result;
+        })
+        .catch((err) => {
+          console.error("Failed to send image", err);
+        });
+    });
   });
 });
