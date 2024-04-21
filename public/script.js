d6154d47cf9d9c3f36e7849a57bbe0fedbba0062
CodingMcnugget <144944897+CodingMcnugget@users.noreply.github.com>
2024-04-21 20:55:20 +0000

    tried to remove the bottom but not working

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", async function () {
   const video = document.getElementById("video");
-  const captureButton = document.getElementById("processImage");
 
   // set up canvas
   const canvas = document.createElement("canvas");
@@ -14,14 +13,14 @@ document.addEventListener("DOMContentLoaded", async function () {
     .catch((err) => {
       console.error("Error accessing the camera", err);
     });
-  captureButton.addEventListener("click", async () => {
+
+  document.body.addEventListener("click", async () => {
     // set the canvas size same as video size
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
-    // dislay the video on canvas
+    // display the video on canvas
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
     // convert the screen shot to base64 URL
-    // const imageDataUrl = canvas.toDataURL("image/jpeg");
     canvas.toBlob(async (blob) => {
       console.log(blob);
       let formData = new FormData();
@@ -29,21 +28,17 @@ document.addEventListener("DOMContentLoaded", async function () {
       // send the image to the server
       await fetch("/api/image", {
         method: "POST",
-        // headers: {
-        //   "Content-Type": "multipart/form-data",
-        // },
-        // body: JSON.stringify({ image: imageDataUrl }),
         body: formData,
       })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.result);
-          const resultImage = document.getElementById("resultImage");
-          resultImage.src = data.result;
-        })
-        .catch((err) => {
-          console.error("Failed to send image", err);
-        });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.result);
+        const resultImage = document.getElementById("resultImage");
+        resultImage.src = data.result;
+      })
+      .catch((err) => {
+        console.error("Failed to send image", err);
+      });
     }, "image/jpeg");
   });
 });
